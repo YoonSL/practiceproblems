c67b908d9e1cbb0a2d4086ac355776f09d0a740b
yoonsub lee <yoonsub90@gmail.com>
2020-01-08 10:27:57 -0500

    codewars even or odd prac

---
+++ evenorodd.js
@@ -0,0 +1,14 @@
+function even_or_odd(number){
+    var tf = "Even";
+    if(number%2 == 1 || number%2 ==-1){
+        tf= "Odd";
+        console.log(tf);
+    }else{
+        console.log(tf);
+    }
+}
+
+even_or_odd(2);
+even_or_odd(0);
+even_or_odd(7);
+even_or_odd(1);
